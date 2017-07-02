c1987f820309ce9a330c8c75467c0febb6fce87f
Hoang-Linh_TO <linhto@HoangLinhTOsAir.net.fpt>
2017-07-02 19:21:28 +0700

    Modify plus_minus for init add

--- plus_minus.c
+++ plus_minus.c
@@ -1,27 +1,10 @@
 /* plus-minus */
 #include <stdio.h>
 
-typedef enum
-{
-    ANFC_SNEP_CONN_DISC = 0U,
-    ANFC_SNEP_CONN_WAIT,
-    ANFC_SNEP_CONN_IDLE,
-    ANFC_SNEP_CONN_RCV_RESPONSE,
-    ANFC_SNEP_CONN_SEND_FRAGMENTS,
-    ANFC_SNEP_CONN_RCV_FRAGMENTS,
-    ANFC_SNEP_CONN_ERROR
-}Anfc_SNEP_ConnState_Type;
-
 int main(void)
 {
     printf("Plus-Minus\n");
 
-    Anfc_SNEP_ConnState_Type CONN_State;
-    
-    CONN_State = ANFC_SNEP_CONN_RCV_RESPONSE;
-    
-    printf("%d", CONN_State);
-    
     int a;
 
     scanf("%d", &a);
