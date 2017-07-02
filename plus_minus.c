b61ff9e614e5d35d1a441cb890dccb217545abed
Hoang-Linh_TO <linhto@HoangLinhTOsAir.net.fpt>
2017-07-02 20:48:13 +0700

    [final] plus_minus

--- plus_minus.c
+++ plus_minus.c
@@ -3,15 +3,42 @@
 
 int main(void)
 {
-    printf("Plus-Minus\n");
+    /*input: the 1st line contains an integer, N, size of the array*/
+    int N;
+    scanf("%d", &N);
 
-    int a;
+    /*input: the 2nd line contains N space-separated integers (a0, a1...)*/
+    int a[N];
+    int index;
+    for (index = 0; index < N; index++)
+    {
+        scanf(" %d", &a[index]);
+    }
 
-    scanf("%d", &a);
-
-    while (a != 1);
-    
-    printf("%d",a);
-    
+    /*output: fraction: positive, negative, zeroes*/
+    int cnt_positive;
+    cnt_positive = 0;
+    int cnt_negative;
+    cnt_negative = 0;
+    int cnt_zeroes;
+    cnt_zeroes = 0;
+    for (index = 0; index < N; index++)
+    {
+        if (a[index] > 0)
+        {
+            cnt_positive++;
+        }
+        if (a[index] < 0)
+        {
+            cnt_negative++;
+        }
+        if (a[index] == 0)
+        {
+            cnt_zeroes++;
+        }
+    }
+    printf("%.6f\n", (float)(cnt_positive)/(float)(N));
+    printf("%.6f\n", (float)(cnt_negative)/(float)(N));
+    printf("%.6f\n", (float)(cnt_zeroes)/(float)(N));
     return 0;
 }
