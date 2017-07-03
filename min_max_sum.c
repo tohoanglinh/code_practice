5212affa5273e589a5578c8d7f32ec580919799a
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-07-03 16:38:55 +0700

    [Final] Warm Up challenges

--- min_max_sum.c
+++ min_max_sum.c
@@ -1,14 +1,17 @@
 /*Mini-Max-Sum*/
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
     /*input: a single line of 5 space-separated integers*/
+    /*input constraints: each integer is in the inclusive range[1, 10^9]*/
     int N, index;
-    long int a[N];
+    long int *a;
     long int sum;
     sum = 0;
     N = 5;
+    a = (long int*)malloc(N*sizeof(long int));
     for (index = 0; index < N; index++)
     {
         scanf(" %ld", &a[index]);
@@ -18,14 +21,30 @@ int main(void)
         }
         sum = sum + a[index];
     }
-    printf("%ld", sum);
-    /*input constraints: each integer is in the inclusive range[1, 109]*/
     /*output: 2 space-separated long integers, min and max values*/    
-    long int partial_sum[N];
-    for (index = 0; index < N; index++)
+    long int *partial_sum;
+    long int min, max;
+    partial_sum = (long int*)malloc(N*sizeof(long int));
+    partial_sum[0] = sum - a[0];
+    min = partial_sum[0];
+    max = partial_sum[0];
+    for (index = 1; index < N; index++)
     {
         partial_sum[index] = sum - a[index];
+        if (max < partial_sum[index])
+        {
+            max = partial_sum[index];
+        }
+        if (min > partial_sum[index])
+        {
+            min = partial_sum[index];
+        }
     }
+    printf("%ld %ld", min, max);
+    
+    /*deallocate pointers*/
+    free(partial_sum);
+    free(a);
     
     return 0;
 }
