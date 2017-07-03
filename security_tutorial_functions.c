60e4666e1498edd6489108068c7c48c28d0ad884
Hoang-Linh_TO <tohoanglinh@gmail.com>
2017-07-03 23:13:46 +0700

    security

---
+++ security_tutorial_functions.c
@@ -0,0 +1,20 @@
+/* challenge: Security Functions
+ * author   : dheeraj
+ * coder    : tohoanglinh 
+ * date     : 3-July-2017 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int func_f1(int x)
+{
+    return (x % 11);
+}
+
+int main(void)
+{
+    /* input */
+    /* output */
+    printf("%d", func_f1(20));
+    return 0;
+}
---
+++ template.c
@@ -0,0 +1,11 @@
+/*challenge:*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    /*input*/
+    /*output*/
+    return 0;
+}
