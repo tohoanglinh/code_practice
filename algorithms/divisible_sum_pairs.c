761647df76c79e478b0ef3a668bc69c53ce85f03
Hoang-Linh_TO <tohoanglinh@gmail.com>
2017-07-09 06:41:04 +0700

    divisible

---
+++ algorithms/divisible_sum_pairs.c
@@ -0,0 +1,21 @@
+/** 
+ * Divisible Sum Pairs
+ * Challenge: Algorithms--Implementation
+ * To Hoang Linh
+ **/
+
+#include <stdio.h>
+
+int main(void)
+{
+    /* input: n, k */
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int i;
+    for (i=0; i<n; i++)
+    {
+        scanf(" %d", &a[i]);
+    }
+
+    return 0;
+}
