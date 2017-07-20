949d47d428fa9053b65fca99db210df9e790715e
Hoang-Linh_TO <tohoanglinh@gmail.com>
2017-07-20 21:54:30 +0700

    once_in_a_tram init

---
+++ w34/once_in_a_tram.c
@@ -0,0 +1,38 @@
+/*
+ * File   : once_in_a_tram.c
+ * Author : To Hoang Linh
+ * Date   : 2017-07-20
+ * ForWhat: One day, Jack was going home by tram. When he got his ticket,
+ * he noticed that number on the ticket was not lucky. A lucky ticket is
+ * a six-digit number on the ticket in which sum of the first three digits
+ * is equal to the sum of the last three digits.
+ * For example, number 165912 is lucky because sum of 1+6+5=12, and
+ * 9+1+2=12.
+ * Since the number on the ticket wasn't lucky, Jack needs your help to find
+ * the next lucky ticket number.
+ * For example, if Jack's ticket number is 165901, then the next lucky ticket
+ * number is 165903.
+ * Given Jack's current ticket number, find and print the next lucky ticket
+ * number.
+ */
+
+/**
+ * INCLUDES
+ */
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/**
+ * DEFINES & MACROS
+ */
+#define MAX_CNT 200
+
+uint8_t main(void)
+{
+    /* input x: integer, 6-digit number on the ticket */
+    int x;
+    /* output */
+    return 0;
+}
