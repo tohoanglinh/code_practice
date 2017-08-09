3711da0a6f546641427df57050899609f72f3d53
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-08-09 17:30:56 +0700

    camelcase_v0

---
+++ algorithms/camelcase.c
@@ -0,0 +1,40 @@
+/**
+ * File   : camelcase.c
+ * Author : To Hoang Linh
+ * Date   : 2017-07-24
+ * ForWhat: Alice wrote a sequence of words in CamelCase as a string of letters,
+ * s , having the following properties:
+ * - It is a concatenation of one or more words consisting of English letters.
+ * - All letters in the first word are lowercase.
+ * - For each of the subsequent words, the first letter is uppercase and rest of
+ * the letters are lowercase.
+ * Given s, print the number of words in s on a new line.
+ */
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define MAX_CNT 200
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+int main(void)
+{
+    /* input */
+    char *s;
+    string_s = malloc(100*sizeof(char));
+    scanf("%s", string_s);
+
+    /* output */
+    free(string_s);
+    return 0;
+}
