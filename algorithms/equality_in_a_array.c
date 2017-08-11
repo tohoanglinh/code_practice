e3960066a42fb312d739302f597716c28071307a
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-08-11 09:37:32 +0700

    'hello_java'

---
+++ algorithms/equality_in_a_array.c
@@ -0,0 +1,34 @@
+/**
+ * File   : equality_in_a_array.c
+ * Author : To Hoang Linh
+ * Date   : 2017-08-11
+ * ForWhat: Karl has an array of n integers defined as A = a0, a1, ..., an-1.
+ * In one operation, he can delete any element from the array.
+ * Karl wants all the elements of the array to be equal to one another. To do
+ * this, he must delete zero or more elements from the array. Find and print
+ * the minimum number of deletion operations Karl must perform so that all the
+ * array's elements are equal.
+ */
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define MAX_CNT 200
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+int main(void)
+{
+    /* input */
+    /* output */
+    return 0;
+}
---
+++ java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld
+{
+    /* This is the first java program.
+     * This will print 'Hello World' string as output.
+     */
+    public static void main(String []args)
+    {
+        System.out.println("Hello World\n");
+    }
+}
