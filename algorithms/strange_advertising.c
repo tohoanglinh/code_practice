d06f9895d2e34a8924a84ace94def04144d64797
Hoang-Linh_TO <tohoanglinh@gmail.com>
2017-08-23 21:37:39 +0700

    strange

--- algorithms/strange_advertising.c
+++ algorithms/strange_advertising.c
@@ -33,15 +33,41 @@ int main(void)
     {
         scanf("%d", &n);
     }
-    
+
     /* output: no of people who liked the ad during the first n days */
-    printf("%f", floor(5/2));
-    
     /*
-    Day1: 5 receiver, floor(5/2) likes
-    Day2: floor(5/2)*3 receiver, floor(6/2) likes
+    Day1: 5 receiver, floor(5/2) likes = floor(receiver/2)
+    Day2: floor(5/2)*3 receiver, floor(6/2) likes = floor(receiver/2)
+    receiver = last_receiver*3;
+    likes = floor(receiver/2);
+    last_receiver = receiver;
     Day3:
     */
-    
+    /* Start with 5 people, after n days, how many people likes that ad? */
+    double receiver, last_receiver;
+    int day;
+    int totallikes;
+    day = 1;
+    double *numlikes;
+    numlikes = malloc(sizeof(double)*n);
+    last_receiver = 5;
+    totallikes = 0;
+    while (day <= n)
+    {
+        /* main code */
+        receiver = last_receiver*3;
+        if (day == 1)
+        {
+            receiver = last_receiver;
+        }
+        numlikes[day] = floor(receiver/2);
+        last_receiver = numlikes[day];
+        totallikes = totallikes + (int)(numlikes[day]);
+        day++;
+    }
+
+    printf("%d", totallikes);
+
+    free(numlikes);
     return 0;
 }
