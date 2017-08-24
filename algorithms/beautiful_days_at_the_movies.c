74f3e3cfafc4eff96f01f5228875e3d65c8f6b84
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-08-24 13:57:50 +0700

    beautiful_days

---
+++ algorithms/beautiful_days_at_the_movies.c
@@ -0,0 +1,37 @@
+/**
+ * File   : beautiful_days_at_the_movies.c
+ * Author : To Hoang Linh
+ * Date   : 2017-08-24
+ * ForWhat: Lily likes to play games with integers and their reversals. For some
+ * integer x, we define reversed(x) to be the reversal of all digits in x. For
+ * example, reversed(123)=321, reversed(21)=12, and reversed(120)=21.
+ * Logan wants to go to the movies with Lily on some day x satisfying i <= x <=j,
+ * but he knows she only goes to the movies on days she considers to be beautiful.
+ * Lily considers a day to be beautiful if the absolute value of the difference
+ * between x and reversed(x) is divisible by k.
+ * Given i, j and k, print the number of beautiful days when Logan and Lily can
+ * go to the movies.
+ */
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define MAX_CNT 200
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+int main(void)
+{
+    /* input */
+    /* output */
+    return 0;
+}
