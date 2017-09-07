f025b8b96b916184a6ae7d9af840ca5c88e1d589
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-09-07 09:00:33 +0700

    pex_v2.1

--- temp/tcpip/parseVersion.java
+++ temp/tcpip/parseVersion.java
@@ -1,76 +1,82 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class parseVersion
 {
-    public static void main(String[] args)
+    /* parse version of FreeRTOS_C55 */
+    public static String[][] parseVersionFreertosC55(Object component, String sdkpath)
     {
+        String sdk_path = System.getenv(sdkpath);
+        String derivative = "FreeRTOS_C55";
+        String freertos_path = sdk_path+"/rtos/"+derivative+"/Source/include/task.h";
+        String version_major = null;
+        String version_minor = null;
+        String version_build = null;
+        String[][] result = new String[2][1];
+
+        /* parse version of FreeRTOS */
+        version_major = parseDefine(freertos_path, "tskKERNEL_VERSION_MAJOR");
+        version_minor = parseDefine(freertos_path, "tskKERNEL_VERSION_MINOR");
+        version_build = parseDefine(freertos_path, "tskKERNEL_VERSION_BUILD");
+
+        /* debug in java */
+        System.out.println("%set @Link1@FreeRtosVersion Value "+version_major+"."+version_minor+"."+version_build);
+
+        /* macro-commands to execute in macro language for processor expert */
+        result[1][0] = "%set @Link1@FreeRtosVersion Value "+version_major+"."+version_minor+"."+version_build;
+        return result;
     }
-    public static String[][] parseVersionLWIP(Object component, String sdk_path)
+
+    /* parse version of lwIP */
+    public static String[][] parseVersionLwip(Object component, String sdkpath)
     {
-        String[][] result = new String[2][2];
-        String s32sdkpath = System.getenv(sdk_path);
+        String sdk_path = System.getenv(sdkpath);
+        String lwip_path = sdk_path+"/middleware/tcpip/lwip/src/include/lwip/init.h";
         String version_major = null;
         String version_minor = null;
         String version_revision = null;
-        try
+        String[][] result = new String[2][1];
+
+        /* parse version of lwIP */
+        version_major = parseDefine(lwip_path, "LWIP_VERSION_MAJOR");
+        version_minor = parseDefine(lwip_path, "LWIP_VERSION_MINOR");
+        version_revision = parseDefine(lwip_path, "LWIP_VERSION_REVISION");
+
+        /* debug in java */
+        System.out.println("%set LWIPVERSION Value "+version_major+"."+version_minor+"."+version_revision);
+
+        /* macro-commands to execute in macro language for processor expert */
+        result[1][0] = "%set LWIPVERSION Value "+version_major+"."+version_minor+"."+version_revision;
+        return result;
+    }
+
+    public static String parseRegExp(String filePath, String patternString)
+    {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath)))
         {
-        	BufferedReader br = new BufferedReader(new FileReader(s32sdkpath+"/middleware/tcpip/lwip/src/include/lwip/init.h"));
-            String line = br.readLine();
-            while (line != null)
+            Pattern pattern = Pattern.compile(patternString);
+            String line = null;
+            while ((line = br.readLine()) != null)
             {
-                if(line.matches("(.*)#define(.*)"+"LWIP_VERSION_MAJOR"+"(.*)[0-9](.*)"))
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find())
                 {
-                    String linep = line.replaceAll("\\s+", "|");
-                    String[] fields = linep.split("\\|");
-                    if (fields.length == 3)
-                    {
-                        version_major = fields[2];
-                    }
-                    else if (fields.length == 4)
-                    {
-                        version_major = fields[3];
-                    }
+                    return matcher.group(1);
                 }
-                if(line.matches("(.*)#define(.*)"+"LWIP_VERSION_MINOR"+"(.*)[0-9](.*)"))
-                {
-                    String linep = line.replaceAll("\\s+", "|");
-                    String[] fields = linep.split("\\|");
-                    if (fields.length == 3)
-                    {
-                        version_minor = fields[2];
-                    }
-                    else if (fields.length == 4)
-                    {
-                        version_minor = fields[3];
-                    }
-                }
-                if(line.matches("(.*)#define(.*)"+"LWIP_VERSION_REVISION"+"(.*)[0-9](.*)"))
-                {
-                    String linep = line.replaceAll("\\s+", "|");
-                    String[] fields = linep.split("\\|");
-                    if (fields.length == 3)
-                    {
-                        version_revision = fields[2];
-                    }
-                    else if (fields.length == 4)
-                    {
-                        version_revision = fields[3];
-                    }
-                }
-                line = br.readLine();
             }
-            br.close();
-            /* macro-commands to execute */
-            result[1][0] = "%set LWIPVERSION Value "+version_major+"."+version_minor+"."+version_revision;
         }
-        catch(Exception e)
+        catch (Exception e)
         {
-            version_major = null;
-            version_minor = null;
-            version_revision = null;
             e.printStackTrace();
         }
-        return result;
+
+        return null;
+    }
+
+    public static String parseDefine(String filePath, String symbol)
+    {
+        return parseRegExp(filePath, "#define\\s+" + symbol + "\\s+([^\\s]+)");
     }
 }
