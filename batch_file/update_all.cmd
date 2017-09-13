e71d3770576640152c48852d3324a1855a5d998f
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-09-13 17:56:23 +0700

    'update'

---
+++ batch_file/update_all.cmd
@@ -0,0 +1,10 @@
+@ECHO OFF
+cls
+REM ************************************************
+REM ** BATCH FILE TO AUTOMATE IN WINDOWS CMD
+REM ** UPDATE ALL GIT REPOSITORY
+REM ** AUTHOR: TO HOANG LINH
+REM ************************************************
+update code_practice && update automation && update doc-internal
+PAUSE
+EXIT
