cac2584369f3e31061fbe0bab45fb81a76090805
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-09-13 17:32:18 +0700

    'update'

---
+++ batch_file/update.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+CLS
+REM ************************************************
+REM ** BATCH FILE TO AUTOMATE GIT REPOS UPDATE BY WINDOWS CMD
+REM ** AUTHOR: TO HOANG LINH
+REM ************************************************
+ECHO Updating GIT Repos...
+cd D:\gitworks\%1
+git fetch && git pull
+git add . && git commit -m 'update' && git push
+PAUSE
+EXIT
