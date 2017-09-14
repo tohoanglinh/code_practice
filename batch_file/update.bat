70543098da5cf98ba346f862103487c5ee3dcc33
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-09-14 08:20:44 +0700

    'batch_update'

--- batch_file/update.bat
+++ batch_file/update.bat
@@ -2,10 +2,11 @@
 REM ************************************************
 REM ** BATCH FILE TO AUTOMATE GIT REPOS UPDATE BY WINDOWS CMD
 REM ** AUTHOR: TO HOANG LINH
+REM ** USAGE : update name_of_git_repo name_commit
 REM ************************************************
 ECHO -----------------------------------------------
 ECHO Updating Git Repos: %1...
 cd /D D:\gitworks\%1
 git fetch && git pull
-git add . && git commit -m 'update' && git push
+git add . && git commit -m '%2' && git push
 PAUSE
--- batch_file/update_all.cmd
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-cls
-REM ************************************************
-REM ** BATCH FILE TO AUTOMATE IN WINDOWS CMD
-REM ** UPDATE ALL GIT REPOSITORY
-REM ** AUTHOR: TO HOANG LINH
-REM ************************************************
-update code_practice & 
-update automation & update doc-internal & update s32_internal_tools & update s32_sdk_vnv_testing & update sdk_codebase
