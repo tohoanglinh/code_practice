bfbcb84d1f0e4c1426569045dbde51e2e4e7df21
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-09-15 09:08:24 +0700

    test

--- batch_file/update.bat
+++ batch_file/update.bat
@@ -7,6 +7,37 @@ REM ************************************************
 ECHO -----------------------------------------------
 ECHO Updating Git Repos: %1...
 cd /D D:\gitworks\%1
-git fetch && git pull
-git add . && git commit -m %2 && git push
+SET gitfolder=0
+IF "%1"=="code_practice" SET gitfolder=1
+
+IF "%1"=="automation" SET gitfolder=2
+IF "%1"=="doc-internal" SET gitfolder=2
+IF "%1"=="s32_internal_tools" SET gitfolder=2
+IF "%1"=="s32_sdk_vnv_testing" SET gitfolder=2
+IF "%1"=="lwip" SET gitfolder=2
+IF "%1"=="wolfssl" SET gitfolder=2
+
+IF "%1"=="sdk_codebase" SET gitfolder=3
+IF "%1"=="tcpip_stack" SET gitfolder=3
+IF "%1"=="anfc_codebase" SET gitfolder=3
+
+IF %gitfolder%==1 (
+  IF "%2" == "" (
+    ECHO empty
+  ) ELSE (
+    ECHO %2
+  )
+  git fetch && git pull
+  git add . && git commit -m %2 && git push
+)
+
+IF %gitfolder%==2 (
+  ECHO Fetching a git repo
+  git fetch && git pull
+)
+
+IF %gitfolder%==3 (
+  ECHO This repo should not be update by this batch file.
+  ECHO Please do it carefully by manual.
+)
 PAUSE
