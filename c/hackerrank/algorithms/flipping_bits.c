c324be1aa456325ae67fb61509f4a8d1960010ee
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-09-21 13:46:24 +0700

    flipping_bits_final

--- c/hackerrank/algorithms/flipping_bits.c
+++ c/hackerrank/algorithms/flipping_bits.c
@@ -11,9 +11,16 @@
  * Includes
  ******************************************************************************/
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdlib.h>
+#include <math.h>
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+unsigned char flip1bit(unsigned long bit);
+unsigned long flipallbits(unsigned long num);
+unsigned long *dec2bin(unsigned long decimal);
+unsigned long bin2dec(unsigned long *binary);
 
 /*******************************************************************************
  * Definitions
@@ -23,31 +30,84 @@
 /*******************************************************************************
  * Code
  ******************************************************************************/
-void wait(void)
+unsigned long flipallbits(unsigned long num)
 {
-    char c;
-    c = getchar();
-    while (getchar() != '\n');
-}
+    int i;
+    unsigned long *binary;
+    unsigned long *flipped_binary;
+    binary = malloc(sizeof(unsigned long)*32);
+    flipped_binary = malloc(sizeof(unsigned long)*32);
 
-unsigned long convert2binary(unsigned long decimal)
-{
-    unsigned long base;
-    unsigned long remainder;
-    unsigned long binary;
+    /* convert decimal to binary in array */
+    binary = dec2bin(num);
 
-    base = 1;
-    binary = 0;
-
-    while (decimal > 0)
+    /* flipping bits */
+    for (i = 31; i >= 0; i--)
     {
-        remainder = decimal % 2;
-        binary = binary + remainder * base;
-        decimal = decimal / 2;
-        base = base * 10;
+        flipped_binary[i] = flip1bit(binary[i]);
     }
 
-    return binary;
+    /* convert binary array to decimal */
+    unsigned long result = bin2dec(flipped_binary);
+
+    free(flipped_binary);
+
+    return result;
+}
+
+unsigned char flip1bit(unsigned long bit)
+{
+    if (1 == bit)
+    {
+        return 0;
+    }
+    else if (0 == bit)
+    {
+        return 1;
+    }
+    else
+    {
+        return (-1);
+    }
+}
+
+unsigned long *dec2bin(unsigned long decimal)
+{
+    static unsigned long bit[32];
+    int i;
+    int quotient;
+
+    /* reset bit element to zeros
+    * Please do not use memset. memset is for character
+    * here is unsigned long --> clear manually
+    */
+    for (i = 0; i < 32; i++)
+    {
+        bit[i] = 0;
+    }
+
+    i = 0;
+    /* convert decimal to binary number in array format */
+    while (decimal > 0)
+    {
+        bit[i] = decimal % 2;
+        i++;
+        quotient = decimal / 2;
+        decimal = quotient;
+    }
+    return bit;
+}
+
+unsigned long bin2dec(unsigned long *binary)
+{
+    int i;
+    unsigned long decimal;
+    decimal = 0;
+    for (i = 31; i >= 0; i--)
+    {
+        decimal = decimal + binary[i]*pow(2,i);
+    }
+    return decimal;
 }
 
 int main(void)
@@ -70,8 +130,10 @@ int main(void)
     }
 
     /* output */
-    printf("%lu", convert2binary(134));
-    //flipbits();
+    for (i = 0; i < T; i++)
+    {
+        printf("%lu\n", flipallbits(*(num+i)));
+    }
 
     free(num);
     return 0;
