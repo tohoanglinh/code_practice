c2574346b3ff5fee45b7dc1971c49d7c26808ab8
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-09-27 17:08:44 +0700

    gcov

---
+++ gcov/header/hello.h
@@ -0,0 +1,7 @@
+#ifndef HELLO_H
+#define HELLO_H
+
+#include <stdio.h>
+#define MAX 500
+
+#endif /* HELLO_H */
---
+++ gcov/header/mathlib.h
@@ -0,0 +1,8 @@
+#ifndef MATHLIB_H
+#define MATHLIB_H
+
+#include <math.h>
+
+int sum(int a, int b);
+
+#endif /* MATHLIB_H */
