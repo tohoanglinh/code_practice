be4e53f033ae99de9cb82577235790bdd85da494
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-10-05 09:50:07 +0700

    clean_code

--- website/jemdoc_mathjax/network-optimization/readme.txt
+++
@@ -1,3 +0,0 @@
-Use package jemdoc_mathjax (Wonseok Shin)
-
-../jemdoc -c mathjax.conf -o html/ *.jemdoc && ../jemdoc -c bibtex.conf -o html/ home.jemdoc
--- website/jemdoc_mathjax/network-optimization/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Compiling network-optimization... :-)"
-../jemdoc -c mathjax.conf -o html/ *.jemdoc && ../jemdoc -c bibtex.conf -o html/ home.jemdoc
-#jemdoc -c mathjax.conf -o html/ *.jemdoc && jemdoc -c bibtex.conf -o html/ home.jemdoc
-echo "DONE!"
