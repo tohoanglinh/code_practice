e17e225cdaff69582e5b897f77afe7941b8d021d
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-10-17 15:48:34 +0700

    remove

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,10 +1,23 @@
 public class HelloWorld
 {
-    /* This is the first java program.
-     * This will print 'Hello World' string as output.
+    /**
+     * (1) Java is Case Sensitivity.
+     * (2) Class Names: HelloWorld (FirstLetterInUpperCase)
+     * (3) Method Names: myMethodName (firstLetterInLowerCase)
+     * (4) Program File Name ==  Class Name\
      */
-    public static void main(String []args)
+    public static void main(String[] args)
     {
-        System.out.println("Hello World\n");
+        System.out.println("Hello World JAVA\n");
+
+        /* this is how to access and use args */
+        // println: print a new line
+        System.out.println(args.length);
+        for (int i = 0; i < args.length; i++)
+        {
+            // printf: print with format
+            System.out.printf("args[%d] = %s\n", i, args[i]);
+        }
+
     }
 }
---
+++ python/pyqt/firstprograms.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt4 tutorial
+
+In this example, we create a simple window in PyQt4.
+
+author: Jan Bodnar.
+website: zetcode.com
+learner: To Hoang Linh.
+"""
+
+import sys
+from PyQt4 import QtGui
+
+def main():
+    print("Hello Qt")
+
+if __name__ == '__main__':
+    main()
