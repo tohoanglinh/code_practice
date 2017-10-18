ab1ecaa2e9b87de8b14429fd179b9c4a773c453d
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2017-10-18 15:38:19 +0700

    csharp_init

---
+++ csharp/HelloWPF/HelloWPF/Greetings.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWPF
+{
+    /// <summary>
+    /// Interaction logic for Greetings.xaml
+    /// </summary>
+    public partial class Greetings : Window
+    {
+        public Greetings()
+        {
+            InitializeComponent();
+        }
+
+        private void Display_Click(object sender, RoutedEventArgs e)
+        {
+            if (helloButton.IsChecked == true)
+            {
+                MessageBox.Show("Hello.");
+            }
+            else if (byeButton.IsChecked == true)
+            {
+                MessageBox.Show("Bye.");
+            }
+        }
+    }
+}
