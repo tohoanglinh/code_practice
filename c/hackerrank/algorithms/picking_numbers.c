b9c3f44f6c7fd6532429023d6590328d3b99fd5c
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2018-07-05 15:43:01 +0700

    picking_num

--- c/hackerrank/algorithms/picking_numbers.c
+++ c/hackerrank/algorithms/picking_numbers.c
@@ -23,30 +23,94 @@
 /*******************************************************************************
  * Code
  ******************************************************************************/
+bool isSubsetSatisfied(int tmp_arr[], int size_tmp_arr)
+{
+	int i, j;
+	for (i = 0; i < size_tmp_arr; i++)
+	{
+		for (j = 0; j < size_tmp_arr; j++)
+		{
+			if (abs(tmp_arr[j]-tmp_arr[i]) > 1)
+			{
+				return false;
+			}				
+		}
+	}
+	return true;
+}
+
+int fact(int n)
+{
+	if (n == 0) return 1;
+	if (n > 0) return n * fact(n-1);
+}
+
+int combinations(int n, int r)
+{
+	if (n == r) return 1;
+	if (r == 0 && n != 0) return 1;
+	else
+	{
+		return (fact(n) / (fact(n-r) * fact(r)));
+	}
+}
+ 
 int main(void)
 {
     printf("Picking Numbers\n");
+       
     /* input n: size of the array */
-    int n;
-    n = 0;
-    while (n < 2 || n > 100)
-    {
-        scanf("%d", &n);
-    }
-    int *arr;
-    arr = malloc(sizeof(int)*n);
-    if (NULL == arr)
-    {
-        return (-1);
-    }
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        scanf(" %d", arr+i);
-    }
+//    int n;
+//    n = 0;
+//    while (n < 2 || n > 100)
+//    {
+//        scanf("%d", &n);
+//    }
+//    
+//    int *arr;
+//    arr = malloc(sizeof(int)*n);
+//    if (NULL == arr)
+//    {
+//        return (-1);
+//    }
+//    
+//	int i;
+//    for (i = 0; i < n; i++)
+//    {
+//        scanf(" %d", arr+i);
+//    }
 
     /* output */
+	/*
+	 * 1. pick a subset ???? HOW TO
+	 * 2. is the subset satisfied? OK
+	 * 3. return number of elements of that subset OK
+	 * 4. continue from 1
+	 */
+	
+	
+	
+//	int max_length = 2;
+//	for (;;)
+//	{
+//		int sub_arr[] = {1, 2, 1, 2};
+//		int sub_arr_size = sizeof(sub_arr)/sizeof(int);
+//		if (true == isSubsetSatisfied(sub_arr, sub_arr_size))
+//		{
+//			printf("OK");
+//			if (max_length < sub_arr_size)
+//			{
+//				max_length = sub_arr_size;
+//			}
+//		}
+//		else
+//		{
+//			printf("NOPE");
+//		}		
+//	}
+//
+//	printf("%d", max_length);
 
-    free(arr);
+//    free(arr);
     return 0;
 }
