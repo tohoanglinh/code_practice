b62c8d39063e16a047b0312681ff4f84060d7f34
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2019-01-09 15:31:48 +0700

    update java

--- java/hackerrank/Solution.java
+++ java/hackerrank/Solution.java
@@ -1,13 +1,49 @@
+import java.util.*;   /* import all from java.util: scanner*/
+
 public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT.
          * Your class should be named Solution.
          */
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
 
-        Scanner scanner = new Scanner(System.in);
+        /* Welcome to Java! */
+        // System.out.println("Hello, World.");
+        // System.out.println("Hello, Java.");
 
+        /* Java Stdin and Stdout I */
+        // Scanner scanner = new Scanner(System.in);
+        // int myInt1 = scanner.nextInt();
+        // int myInt2 = scanner.nextInt();
+        // int myInt3 = scanner.nextInt();
+        // scanner.close();
+        // System.out.println(myInt1);
+        // System.out.println(myInt2);
+        // System.out.println(myInt3);
+
+        /* Java If-Else */
+        Scanner scan = new Scanner(System.in);
+        int positiveInteger = scan.nextInt();
+        scan.close();
+
+        if ((positiveInteger % 2) != 0)
+        {
+            System.out.println("Weird");
+        }
+        else
+        {
+            if ((positiveInteger >=2 ) && (positiveInteger <= 5))
+            {
+                System.out.println("Not Weird");
+            }
+            else if ((positiveInteger >=6 ) && (positiveInteger <= 20))
+            {
+                System.out.println("Weird");
+            }
+            else if (positiveInteger > 20)
+            {
+                System.out.println("Not Weird");
+            }
+        }
     }
 }
