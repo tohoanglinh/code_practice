63b1b191436d3e2557c14ebd516d98a995f67fc0
Linh To Hoang-nxf31152 <linh.tohoang@nxp.com>
2019-04-23 10:16:31 +0700

    add printing_token

--- c/hackerrank/algorithms/printing_tokens.c
+++ c/hackerrank/algorithms/printing_tokens.c
@@ -1,44 +1,54 @@
+/**
+ * File   : printing_tokens.c
+ * Author : To Hoang Linh
+ * Date   : 2019-04-23
+ * ForWhat: Printing Tokens
+ * Given a sentence, s, print each word of the sentence in a new line
+ */
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_STRING_LENGTH   (100)
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define MAX_STRING_LENGTH   (500)
 
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
 int main(void)
 {
-    /*
-    * Printing Tokens
-    * Given a sentence, s, print each word of the sentence in a new line
-    */
-
-    char input_string[MAX_STRING_LENGTH];
+    char *input_string;
     int i;
-    int j;
-    char word[MAX_STRING_LENGTH];
-    int start_char = 0;
-    int end_char = 0;
+
+    input_string = (char *)malloc(sizeof(char)*MAX_STRING_LENGTH);
+
+    if (input_string == NULL)
+    {
+        printf("Failed to allocate memory\n");
+        return 1;
+    }
 
     scanf("%[^\n]s", input_string);
 
-    // printf("Length of string = %d\n", strlen(input_string));
-
     for (i = 0; i < strlen(input_string); i++)
     {
-        if ((' ' == input_string[i]) || ('\0' == input_string[i]))
+        if (' ' == input_string[i])
         {
-            // printf("found a word\n");
-            end_char = i;
-            // start_char -> end_char
-            for (j = start_char; j < end_char; j++)
-            {
-                word[j - start_char] = input_string[j];
-            }
-            word[j+1] = '\0';
-            printf(word);
             printf("\n");
-            start_char = end_char+1;
+        }
+        else
+        {
+            printf("%c", input_string[i]);
         }
     }
+
+    free(input_string);
 
     return 0;
 }
