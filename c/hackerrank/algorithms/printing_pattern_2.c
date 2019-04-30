0b56bcf6b8863c4c10e7f3813eb2ac48d4b5a53a
ToHoangLinh <tohoanglinh@gmail.com>
2019-04-30 12:35:06 +0700

    Update printing_pattern_2.c

--- c/hackerrank/algorithms/printing_pattern_2.c
+++ c/hackerrank/algorithms/printing_pattern_2.c
@@ -1,8 +1,9 @@
 /**
- * File   : template.c
+ * File   : printing_pattern_2.c
  * Author : To Hoang Linh
  * Date   : 2017-08-28
- * ForWhat: to create a generic template for C/C++ source coding.
+ * ForWhat: print the pattern of the following form (squares)
+ * containing the numbers from 1 to n.
  */
 
 /*******************************************************************************
@@ -22,21 +23,17 @@
 /*******************************************************************************
  * Code
  ******************************************************************************/
-void wait(void)
+void draw_pattern(int **array, int dim)
 {
-    char c;
-    c = getchar();
-    while (getchar() != '\n');
-}
+    int r, c;
 
-void draw_square(int num)
-{
-    /* draw a square with input number */
-    int i;
-
-    for (i = 0; i < num; i++)
+    for (r = 0; r < dim; r++)
     {
-         printf("%d", num);
+        for (c = 0; c < dim; c++)
+        {
+            printf("%d ", array[r][c]);
+        }
+        printf("\n");
     }
 }
 
@@ -44,10 +41,50 @@ int main(void)
 {
     /* input */
     int n;
+    int m;
+    int i;
+    int j;
+    int length;
 
     scanf("%d", &n);
 
-    printf("Gia tri n = %d\n", n);
+    length = (2*n - 1);
+
+    /* memory allocation for two-dimension array */
+    int **arr = (int **)malloc(length * sizeof(int *));
+    for (i = 0; i < length; i++)
+    {
+        arr[i] = (int *)malloc(length * sizeof(int));
+    }
+
+    /* set data at each cell location */
+    m = 0;
+    while (m <= (length / 2))
+    {
+        for (i = m; i < length-m; i++)
+        {
+            for (j = m; j < length-m; j++)
+            {
+                if ((m == i) || (m == j) || ((length-m-1) == i) || ((length-m-1) == j))
+                {
+                    arr[i][j] = n;
+                }
+            }
+        }
+        m++;
+        n--;
+    }
+
+    /* draw pattern */
+    draw_pattern(arr, length);
+
+    /* free allocated memory */
+    for (i = 0; i < length; i++)
+    {
+        free(arr[i]);
+    }
+
+    free(arr);
 
     /* output */
     return 0;
